28ea49d51a6d0cc94fda2fdee1855179780eb7c2
Daniel Granger <dg1234uk@me.com>
2023-06-30 22:19:45 +0100

    README updated

--- garmin-fpl-to-skydemon-wpts.js
+++ garmin-fpl-to-skydemon-wpts.js
@@ -2,6 +2,7 @@ import path from 'path';
 import xml2js from 'xml-js';
 import {
   ensureDirectoryExists,
+  filterFilesByExtension,
   getInputFiles,
   isValidGarminFplJsonStructure,
   logError,
@@ -12,34 +13,85 @@ import {
 const DEFAULT_INPUT_DIRECTORY = './input';
 const DEFAULT_OUTPUT_DIRECTORY = './output';
 
+export function convertXmlToJson(inputXml, file) {
+  try {
+    const inputJson = JSON.parse(xml2js.xml2json(inputXml, { compact: true }));
+    return inputJson;
+  } catch (err) {
+    logError(`Error parsing the XML: ${err.message} in file : ${file}`);
+    return null;
+  }
+}
+
 // Extracts waypoints from input JSON
 export function extractWaypoints(inputJson) {
-  const waypoints = inputJson['flight-plan']['waypoint-table']['waypoint'];
+  const waypoints =
+    inputJson['flight-plan']['waypoint-table']['waypoint'] || [];
+  if (waypoints.length === 0) {
+    logError('No waypoints found in the input file');
+  }
   return waypoints;
 }
 
-// Core function to process a single file
-async function processFile(inputDirectory, outputDirectory, file) {
-  const inputFile = path.join(inputDirectory, file);
+// Removes duplicate waypoints
+export function removeDuplicates(waypoints) {
+  return Array.from(new Set(waypoints.map(JSON.stringify))).map(JSON.parse);
+}
 
-  // Skip non .fpl files
-  if (path.extname(file).toLowerCase() !== '.fpl') {
-    logError(`Skipping non-fpl file: ${file}`);
-    return;
-  }
+// Sorts waypoints alphabetically by name
+export function sortWaypoints(uniqueWaypoints) {
+  uniqueWaypoints.sort((a, b) => {
+    if (a.identifier._text < b.identifier._text) {
+      return -1;
+    }
+    if (a.identifier._text > b.identifier._text) {
+      return 1;
+    }
+    return 0;
+  });
+
+  return uniqueWaypoints;
+}
+
+// Constructs the output JSON structure
+export function constructOutputJson(waypoints) {
+  const output = {
+    _declaration: { _attributes: { version: '1.0', encoding: 'utf-8' } },
+    gpx: {
+      _attributes: {
+        'xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
+        'xsi:schemaLocation':
+          'http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd',
+        version: '1.1',
+        creator: 'SkyDemon',
+        xmlns: 'http://www.topografix.com/GPX/1/1',
+      },
+      wpt: waypoints.map((waypoint) => ({
+        _attributes: {
+          lat: waypoint.lat._text,
+          lon: waypoint.lon._text,
+        },
+        name: { _text: waypoint.identifier._text },
+        sym: { _text: 'Circle' },
+        extensions: {
+          identifier: { _text: '' },
+          category: { _text: '' },
+        },
+      })),
+    },
+  };
+  return output;
+}
+
+// Core function to process a single file
+async function processFile(inputDirectory, file) {
+  const inputFile = path.join(inputDirectory, file);
 
   // Read input file
   const inputXml = await readFile(inputFile);
   if (!inputXml) return;
 
-  // Convert XML to JSON
-  let inputJson;
-  try {
-    inputJson = JSON.parse(xml2js.xml2json(inputXml, { compact: true }));
-  } catch (err) {
-    logError(`Error parsing the XML: ${err.message} in file : ${file}`);
-    return;
-  }
+  const inputJson = convertXmlToJson(inputXml, file);
 
   // Validate JSON structure
   if (!isValidGarminFplJsonStructure(inputJson)) {
@@ -49,10 +101,6 @@ async function processFile(inputDirectory, outputDirectory, file) {
 
   // Extract waypoints
   const waypoints = extractWaypoints(inputJson);
-  if (!waypoints) {
-    logError('Unexpected JSON structure: waypoint information');
-    return;
-  }
 
   return waypoints;
 }
@@ -71,54 +119,23 @@ async function main() {
   // Get the list of files and process each file
   const files = await getInputFiles(inputDirectory);
 
+  const fplFiles = filterFilesByExtension(files, '.fpl');
+
   let allWaypoints = [];
-  for (const file of files) {
-    const waypoints = await processFile(inputDirectory, outputDirectory, file);
-    allWaypoints = allWaypoints.concat(waypoints);
+  for (const file of fplFiles) {
+    const waypoints = await processFile(inputDirectory, file);
+    if (waypoints) {
+      allWaypoints = allWaypoints.concat(waypoints);
+    }
   }
 
   // Remove duplicates
-  const uniqueWaypoints = Array.from(
-    new Set(allWaypoints.map(JSON.stringify))
-  ).map(JSON.parse);
+  const uniqueWaypoints = removeDuplicates(allWaypoints);
 
   // Sort waypoints alphabetically by name
-  uniqueWaypoints.sort((a, b) => {
-    if (a.identifier._text < b.identifier._text) {
-      return -1;
-    }
-    if (a.identifier._text > b.identifier._text) {
-      return 1;
-    }
-    return 0;
-  });
+  const sortedWaypoints = sortWaypoints(uniqueWaypoints);
 
-  // Create a new JSON structure for the output XML
-  const output = {
-    _declaration: { _attributes: { version: '1.0', encoding: 'utf-8' } },
-    gpx: {
-      _attributes: {
-        'xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
-        'xsi:schemaLocation':
-          'http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd',
-        version: '1.1',
-        creator: 'SkyDemon',
-        xmlns: 'http://www.topografix.com/GPX/1/1',
-      },
-      wpt: uniqueWaypoints.map((waypoint) => ({
-        _attributes: {
-          lat: waypoint.lat._text,
-          lon: waypoint.lon._text,
-        },
-        name: { _text: waypoint.identifier._text },
-        sym: { _text: 'Circle' },
-        extensions: {
-          identifier: { _text: '' },
-          category: { _text: '' },
-        },
-      })),
-    },
-  };
+  const output = constructOutputJson(sortedWaypoints);
 
   // Convert the output JSON back to XML
   const outputXml = xml2js.json2xml(output, { compact: true, spaces: 2 });
@@ -130,5 +147,6 @@ async function main() {
 
 // Start the program and catch any top-level errors
 main().catch((err) => {
-  logError('An error occurred:', err.message);
+  const errorMessage = err.message || 'An unknown error occurred';
+  logError('An error occurred:', errorMessage);
 });

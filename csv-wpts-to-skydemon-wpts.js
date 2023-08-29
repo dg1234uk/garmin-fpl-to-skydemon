79f5982025aeb5ddbaa1c3bf32bf9dae85acfab2
Daniel Granger <dg1234uk@me.com>
2023-08-29 22:12:55 +0100

    csv-wpts-to-skydemon-wpts added

--- csv-wpts-to-skydemon-wpts.js
+++ csv-wpts-to-skydemon-wpts.js
@@ -1,10 +1,10 @@
 import path from "path";
 import xml2js from "xml-js";
+import Papa from "papaparse";
 import {
   ensureDirectoryExists,
   filterFilesByExtension,
   getInputFiles,
-  isValidGarminFplJsonStructure,
   logError,
   readFile,
   writeFile,
@@ -13,21 +13,73 @@ import {
 const DEFAULT_INPUT_DIRECTORY = "./input";
 const DEFAULT_OUTPUT_DIRECTORY = "./output";
 
-// Convert XML to JSON
-// If the XML is invalid, log an error and return null
-export function convertXmlToJson(inputXml, file) {
-  try {
-    const inputJson = JSON.parse(xml2js.xml2json(inputXml, { compact: true }));
-    return inputJson;
-  } catch (err) {
-    logError(`Error parsing the XML: ${err.message} in file : ${file}`);
+// Convert CSV to JSON
+export function convertCsvToJson(inputCsv) {
+  const results = Papa.parse(inputCsv, {
+    header: true,
+    dynamicTyping: true,
+    skipEmptyLines: true,
+  });
+  if (results.errors.length) {
+    logError("Error parsing the CSV:", results.errors);
     return null;
   }
+  return results.data;
+}
+
+/**
+ * Converts a coordinate string in various formats (DMS, DM.m, D.d) to decimal degrees.
+ * @param {string} coord - The coordinate string (e.g., 412412.2N, 4124.202N, 41.4034N, 412412N).
+ * @returns {number|null} The coordinate in decimal degrees or null if format is not recognized.
+ */
+function toDecimalDegrees(coord) {
+  // Define a list of expected patterns for various coordinate formats.
+  const patterns = [
+    { format: "DMSs", pattern: /^(\d{1,3})(\d{2})(\d{2}\.\d+)([NSEW])$/ }, // DMS with decimal seconds (e.g., 412412.2N)
+    { format: "DMm", pattern: /^(\d{1,3})(\d{2}\.\d+)([NSEW])$/ }, // DM with decimal minutes (e.g., 4124.202N)
+    { format: "Dd", pattern: /^(\d+\.\d+)([NSEW])$/ }, // Pure decimal (e.g., 41.4034N)
+    { format: "DMS", pattern: /^(\d{1,3})(\d{2})(\d{2})([NSEW])$/ }, // Standard DMS without any decimal (e.g., 412412N)
+  ];
+
+  // Initialize the variable to store the converted value.
+  let decimal = null;
+
+  // Loop through each pattern and attempt to match the input coordinate.
+  patterns.forEach((patternObj) => {
+    const match = coord.match(patternObj.pattern);
+    // If a match is found and the value hasn't been set yet...
+    if (match && decimal === null) {
+      // Extract and parse degree, minute, and second components.
+      const deg = parseFloat(match[1]);
+      const min = parseFloat(match[2] || 0);
+      const sec = parseFloat(match[3] || 0);
+
+      // Determine the direction (N, S, E, W) to decide if value should be negative.
+      const dir = match[4] || match[5]; // Adjust for patterns that might have different group counts
+
+      // Calculate the decimal degree value.
+      decimal = deg + min / 60 + sec / 3600;
+
+      // If the direction is South or West, the value should be negative.
+      if (dir === "S" || dir === "W") decimal *= -1;
+    }
+  });
+
+  // Return the converted decimal degree value (or null if no format matched).
+  return decimal;
 }
 
 // Extracts waypoints from input JSON
 export function extractWaypoints(inputJson) {
-  const waypoints = inputJson["flight-plan"]["waypoint-table"].waypoint || [];
+  const waypoints = inputJson.map((item) => {
+    const DdLat = toDecimalDegrees(item.latitude);
+    const DdLon = toDecimalDegrees(item.longitude);
+    return {
+      lat: { _text: DdLat },
+      lon: { _text: DdLon },
+      identifier: { _text: item.name },
+    };
+  });
   if (waypoints.length === 0) {
     logError("No waypoints found in the input file");
   }
@@ -90,16 +142,12 @@ async function processFile(inputDirectory, file) {
   const inputFile = path.join(inputDirectory, file);
 
   // Read input file
-  const inputXml = await readFile(inputFile);
-  if (!inputXml) return [];
+  const inputCsv = await readFile(inputFile);
+  if (!inputCsv) return [];
 
-  const inputJson = convertXmlToJson(inputXml, file);
+  const inputJson = convertCsvToJson(inputCsv);
 
-  // Validate JSON structure
-  if (!isValidGarminFplJsonStructure(inputJson)) {
-    logError("Unexpected JSON structure");
-    return [];
-  }
+  // TODO: Validate the input CSV structure
 
   // Extract waypoints
   const waypoints = extractWaypoints(inputJson);
@@ -108,16 +156,16 @@ async function processFile(inputDirectory, file) {
 }
 
 // Process all files in the input directory
-// Filters for .fpl files, processes them, removes duplicate waypoints, sorts waypoints,
-// constructs output JSON, converts it back to XML, and writes it to a file in the output directory
+// Filters for .csv files, processes them, removes duplicate waypoints, sorts waypoints,
+// constructs output JSON, converts it to XML, and writes it to a file in the output directory
 export async function processFiles(inputDirectory, outputDirectory) {
   const files = await getInputFiles(inputDirectory);
 
-  const fplFiles = filterFilesByExtension(files, ".fpl");
+  const csvFiles = filterFilesByExtension(files, ".csv");
 
   let allWaypoints = [];
 
-  const waypointPromises = fplFiles.map(async (file) => {
+  const waypointPromises = csvFiles.map(async (file) => {
     const waypoints = await processFile(inputDirectory, file);
     return waypoints || []; // Ensure we always return an array, even if waypoints is undefined
   });
@@ -139,7 +187,7 @@ export async function processFiles(inputDirectory, outputDirectory) {
   // Convert the output JSON back to XML
   const outputXml = xml2js.json2xml(output, { compact: true, spaces: 2 });
   // Write the output to a file
-  const outputFilePath = path.join(outputDirectory, "fpl-waypoints.gpx");
+  const outputFilePath = path.join(outputDirectory, "csv-waypoints.gpx");
 
   await writeFile(outputFilePath, outputXml);
 }
